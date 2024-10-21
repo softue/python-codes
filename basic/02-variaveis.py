642e08e52856b266908e5a30ef8950468f1e3e62
softue <romulomendesfigueiredo@gmail.com>
2024-10-21 10:06:51 -0300

    criar variáveis

---
+++ basic/02-variaveis.py
@@ -0,0 +1,11 @@
+"""
+# Python Variables
+
+In Python, variables are created when you assign a value to it.
+Python has no command for declaring a variable.
+"""
+x = 5
+y = "Olá, mundo!"
+
+print(x)
+print(y)
